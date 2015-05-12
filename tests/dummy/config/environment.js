03c1edb05a4bc39185052b8ef9e18b652d06564f
Ricardo Mercado <ricky.mercado@cometa.works>
2015-05-12 16:06:44 -0400

    Remove API Key

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -13,7 +13,7 @@ module.exports = function(environment) {
       }
     },
 
-    bingAPI: 'AkmMkZ5YpX8xsXHY4uBxD8Gz2S5f3GkTRebOw0t4voyb7gFryc0ElW4toY3cJbTt',
+    bingAPI: 'INSERT BING API',
 
     APP: {
       // Here you can pass flags/options to your application instance
