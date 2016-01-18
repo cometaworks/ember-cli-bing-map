0313819bf4b843a0364e5c96f0b02896c026679b
futoricky <ricardoamercado@gmail.com>
2016-01-18 00:16:41 -0400

    final ember update touches

--- tests/dummy/app/components/bing-component.js
+++ tests/dummy/app/components/bing-component.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 import layout from '../templates/components/bing-component';
 
-function latRandomizer(){
+function latRandomizer() {
   return (Math.random() * 181) - 90;
 }
 
 function lngRandomizer() {
-  return (Math.random() * 281) -180;
+  return (Math.random() * 281) - 180;
 }
 
 export default Ember.Component.extend({
@@ -18,30 +18,30 @@ export default Ember.Component.extend({
   markers: [
     {
       lat: latRandomizer(),
-      lng: lngRandomizer()  
-    }          
+      lng: lngRandomizer(),
+    },
   ],
 
   polygonLocation: {
     location1:{
       lat: latRandomizer(),
-      lng: lngRandomizer()
-    }, 
+      lng: lngRandomizer(),
+    },
     location2:{
       lat: latRandomizer(),
-      lng: lngRandomizer()
+      lng: lngRandomizer(),
     },
     location3:{
       lat: latRandomizer(),
-      lng: lngRandomizer()
+      lng: lngRandomizer(),
     },
     location4:{
       lat: latRandomizer(),
-      lng: lngRandomizer()
-    }
+      lng: lngRandomizer(),
+    },
   },
 
   fillColor: [Math.floor(Math.random() * 250), Math.floor(Math.random() * 250), Math.floor(Math.random() * 250), Math.floor(Math.random() * 250)],
 
-  strokeColor: [Math.floor(Math.random() * 250), Math.floor(Math.random() * 250), Math.floor(Math.random() * 250), Math.floor(Math.random() * 250)]
+  strokeColor: [Math.floor(Math.random() * 250), Math.floor(Math.random() * 250), Math.floor(Math.random() * 250), Math.floor(Math.random() * 250)],
 });
